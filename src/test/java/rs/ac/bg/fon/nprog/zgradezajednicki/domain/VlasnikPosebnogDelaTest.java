eb9fa7ae3228201ce68317e044660ed4744b0a55
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-06-06 12:45:47 +0200

    Kreirani testovi za klasu VlasnikPosebnogDela.

--- src/test/java/rs/ac/bg/fon/nprog/zgradezajednicki/domain/VlasnikPosebnogDelaTest.java
+++ src/test/java/rs/ac/bg/fon/nprog/zgradezajednicki/domain/VlasnikPosebnogDelaTest.java
@@ -8,62 +8,106 @@ import org.junit.jupiter.api.Test;
 
 class VlasnikPosebnogDelaTest extends GenericEntityTest {
 
+	private VlasnikPosebnogDela v;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		genericEntity = new VlasnikPosebnogDela();
+		v = new VlasnikPosebnogDela();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		genericEntity = null;
+		v=null;
 	}
 
 	@Test
 	void testVlasnikPosebnogDela() {
-		fail("Not yet implemented");
+		v = new VlasnikPosebnogDela();
+		
+		assertNotNull(v);
 	}
 
 	@Test
 	void testVlasnikPosebnogDelaLongStringStringStringDoubleStringStambenaZajednica() {
-		fail("Not yet implemented");
+		v = new VlasnikPosebnogDela(1l, "Pera", "Peric", "12", 43, "pera@gmail.com", new StambenaZajednica());
+		
+		assertNotNull(v);
+		assertEquals(1l, v.getVlasnikId());
+		assertEquals("Pera", v.getIme());
+		assertEquals("Peric", v.getPrezime());
+		assertEquals("12", v.getBrojPosebnogDela());
+		assertEquals(43, v.getVelicinaPosebnogDela());
+		assertEquals("pera@gmail.com", v.getKontaktVlasnika());
+		assertNotNull(v.getStambenaZajednica());
 	}
 
 	@Test
 	void testSetStambenaZajednica() {
-		fail("Not yet implemented");
+		v.setStambenaZajednica(new StambenaZajednica());
+		
+		assertNotNull(v.getStambenaZajednica());
 	}
 
 	@Test
 	void testSetVlasnikId() {
-		fail("Not yet implemented");
+		v.setVlasnikId(3l);
+		
+		assertEquals(3l,v.getVlasnikId());
 	}
 
 	@Test
 	void testSetIme() {
-		fail("Not yet implemented");
+		v.setIme("Mika");
+		
+		assertEquals("Mika",v.getIme());
 	}
 
 	@Test
 	void testSetPrezime() {
-		fail("Not yet implemented");
+		v.setPrezime("Mikic");
+		
+		assertEquals("Mikic",v.getPrezime());
 	}
 
 	@Test
 	void testSetBrojPosebnogDela() {
-		fail("Not yet implemented");
+		v.setBrojPosebnogDela("14");
+		
+		assertEquals("14",v.getBrojPosebnogDela());
 	}
 
 	@Test
 	void testSetVelicinaPosebnogDela() {
-		fail("Not yet implemented");
+		v.setVelicinaPosebnogDela(55);
+		
+		assertEquals(55,v.getVelicinaPosebnogDela());
 	}
 
 	@Test
 	void testSetKontaktVlasnika() {
-		fail("Not yet implemented");
+		v.setKontaktVlasnika("mika@gmail.com");
+		
+		assertEquals("mika@gmail.com",v.getKontaktVlasnika());
 	}
 
 	@Test
 	void testSetMernaJedinica() {
-		fail("Not yet implemented");
+		v.setMernaJedinica(MernaJedinica.m2);
+		
+		assertEquals(MernaJedinica.m2,v.getMernaJedinica());
 	}
 
+	@Test
+	void testToString() {
+		v.setIme("Zika");
+		v.setPrezime("Zikic");
+		
+		String s = v.toString();
+		
+		assertTrue(s.contains("Zika"));
+		assertTrue(s.contains("Zikic"));
+		
+	}
 }
